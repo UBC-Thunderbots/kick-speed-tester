be0e9dd402c16f5f93a5cf9ff12e5f7951a4d25d
Musa <91393150+Musach@users.noreply.github.com>
2023-05-06 15:13:02 -0700

    Added kickspeed tester arduino file to repo

---
+++ kickspeedTester/kickspeedTester.ino
@@ -0,0 +1,74 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_GrayOLED.h>
+#include <Adafruit_SPITFT.h>
+#include <Adafruit_SPITFT_Macros.h>
+
+
+
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+double startTime = 0; //Initialize the start time
+double endTime = 0;  //Initialize the end time
+double timeTakenInSeconds = 0; //Difference between start and end time
+double speedOfObject = 0;  //Holds the value distance divided by time taken
+const double distance = 0.01;  // Distance between the two sensors is set to 1 metre
+int executed = 0;   //Flag to run the code in the loop only once. When set to 1, code in the loop is not executed.
+int sensor1 = 3;   //idk which pin to put it in, so I assigned it at random
+int sensor2 = 5; 
+int sense1 = 0;
+int sense2 = 0;
+
+void setup() {
+  
+  Serial.begin(115200);
+
+  pinMode(sensor1, INPUT);
+  pinMode(sensor2, INPUT);
+  //The pinMode() function is used to configure a specific pin to behave either as an input or an output. 
+
+  }
+
+void loop() {
+ 
+
+    if (digitalRead(sensor1) == 1) { //If movement is detected by the first sensor     
+        sense1 = 1;
+
+    } 
+    if (sense1 == 1 && digitalRead (sensor1) == 0){
+      startTime = millis(); //Assign Arduino time to startTime 
+      sense1 = 0;
+    }
+    
+    
+  if (digitalRead(sensor2) == 1) { //If movement is detected by the second sensor
+      sense2 = 1;
+  }
+  if (sense2 == 1 && digitalRead(sensor2)==0){
+        endTime = millis ();
+        sense2 = 0;
+      timeTakenInSeconds = (endTime - startTime)*1000;
+      speedOfObject = distance / timeTakenInSeconds; //Calculate speed in metres per second
+
+        /* Print the values to the serial monitor */
+        Serial.print("Start Time: "); //Print to the serial monitor
+        Serial.print(startTime*1000);  //Print value
+        Serial.println(" seconds");
+        Serial.print("End Time: "); //Print value
+        Serial.print(endTime*1000);    //Print value
+        Serial.println(" seconds");
+        Serial.print("Speed of Object = ");
+        Serial.print(speedOfObject);
+        Serial.println(" m/s");
+        Serial.println(" ");
+        // grd to neg and pwr to pos
+  }
+      
+      
+}
+
+
+
+
+
